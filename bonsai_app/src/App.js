a3c0a2b686b910127fc3e225684b6adb30ee8ebd
alexanderesp98 <alexbcn98@gmail.com>
2022-06-05 21:36:09 +0200

    create project and integration of SASS

---
+++ bonsai_app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+//import './App.css';
+import './prueba.scss'
+function App() {
+  return (
+    <div className="App">
+      <h1>Bonsai App</h1>
+      <div className="caja">
+        <h2>Encabezado</h2>
+        <p>Hola 1</p>
+      </div>
+      <div className="caja">
+        <h2>Encabezado</h2>
+        <p>Hola 2</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
